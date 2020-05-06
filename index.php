2ccf293f3e85639f1e928465cbe01bc1832be278
Marcin Bazylak <62428212+MarcinBazylak@users.noreply.github.com>
2020-05-06 13:15:17 +0200

    initial

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+   <meta charset="UTF-8">
+   <link rel="stylesheet" href="css/style.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Pizzeria Francesco - Najlepsza PIZZA w mieście</title>
+</head>
+<body>
+
+   <div class="container">
+   
+      <header>
+         <h1><span style="font-size:8vh">FRANCESCO</span><br>NAJLEPSZA PIZZA W MIEŚCIE</h1>
+         <nav>
+            <ul>
+               <li>
+                  <a href="index.php">Strona główna</a>
+               </li>
+               <li>
+                  <a href="cennik">Cennik</a>
+               </li>
+               <li>
+                  <a href="zamow">Złóż zamówienie</a>
+               </li>
+               <li>
+                  <a href="kontakt">Kontakt</a>
+               </li>
+            </ul>
+         </nav>
+      </header>
+
+      <aside>
+      ASIDE
+      </aside>
+
+      <main>
+      MAIN
+      </main>
+
+   </div>
+
+   <footer>
+      FOOTER
+   </footer>
+   
+</body>
+</html>
